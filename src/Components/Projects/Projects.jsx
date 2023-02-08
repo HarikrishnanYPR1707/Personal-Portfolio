ae9139e64c53d7afe6e9c542c3e79477e6d64be3
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-02-08 23:45:36 +0530

    Completed the Projects title

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 const Projects = () => {
   return (
     <section className="p-6 m-5 bg-[#282828] rounded-lg flex flex-col justify-center items-center md:w-5/6 md:mx-auto lg:w-5/6 ">
-        <h1 className="text-white text-center font-swad text-5xl uppercase">
+        <h1 className="text-white text-center font-swad text-5xl uppercase border-2 border-white ">
             Projects
         </h1>
-        <div className="">
-            
+        <div className="border-2 border-red-500 mt-5">
+            <div className="">
+              Harikrishnan
+            </div>
         </div>
     </section>
   )
