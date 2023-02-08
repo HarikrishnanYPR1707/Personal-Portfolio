3c34429010efe5333baf7994f22ff342af8697af
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-02-09 00:11:32 +0530

    Edited the structure of the card

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -11,16 +11,16 @@ const Projects = () => {
 					{/* <div className="">
 						<img src="" alt="Logo-Project-1" className="" />
 					</div> */}
-					<div className="">
-						<h2 className="">
-							Country Guide Website
-						</h2>
-						<button className="">
-							<a href="https://github.com/HarikrishnanYPR1707" target="_blank">
-								View This Website
-							</a>
-						</button>
-					</div>
+					{/* <div className=""> */}
+					<h2 className="">
+						Country Guide Website
+					</h2>
+					<button className="">
+						<a href="https://github.com/HarikrishnanYPR1707" target="_blank">
+							View This Website
+						</a>
+					</button>
+					{/* </div> */}
 				</div>
 			</div>
 		</section>
