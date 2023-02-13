df283f6cf166b6ed05310954e6e43e46bee87a81
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-02-13 08:06:03 +0530

    completed the structure of project section

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,30 +1,19 @@
-import React from 'react'
+import React from 'react';
+import Card from './card';
 
 const Projects = () => {
 	return (
-		<section className="p-6 m-5 bg-[#282828] rounded-lg flex flex-col justify-center items-center md:w-5/6 md:mx-auto lg:w-5/6 ">
-			<h1 className="text-white text-center font-swad text-5xl uppercase border-2 border-white ">
+		<section className="p-10 m-5 bg-[#282828] rounded-lg flex flex-col justify-center items-center md:w-5/6 md:mx-auto lg:w-5/6 ">
+			<h1 className="text-white text-center font-swad text-5xl uppercase">
 				Projects
 			</h1>
-			<div className="border-2 border-red-500 mt-5">
-				<div className="w-96">
-					{/* <div className="">
-						<img src="" alt="Logo-Project-1" className="" />
-					</div> */}
-					<div className="">
-						<h2 className="font-swad text-white text-2xl">
-							Country Guide Website
-						</h2>
-						<p className="">
-							Description
-						</p>
-						<button className="">
-							<a href="https://github.com/HarikrishnanYPR1707" target="_blank">
-								View This Website
-							</a>
-						</button>
-					</div>
-				</div>
+			<div className="md:grid md:grid-cols-2 lg:grid lg:grid-cols-3 lg:gap-4">
+				<Card name="Country Guide Website" desc="This is a website where you can see the basic detail of any country you can think of." link="https://cguide.netlify.app/" />
+				<Card name="QRCode Generator Website" desc="This is a website which can be used for generating qrcode of any links with various sizes." link="https://hqrgenerator.netlify.app/" />
+				<Card name="QRCode Generator Website" desc="This is a website which can be used for generating qrcode of any links with various sizes." link="https://hqrgenerator.netlify.app/" />
+				<Card name="QRCode Generator Website" desc="This is a website which can be used for generating qrcode of any links with various sizes." link="https://hqrgenerator.netlify.app/" />
+				<Card name="QRCode Generator Website" desc="This is a website which can be used for generating qrcode of any links with various sizes." link="https://hqrgenerator.netlify.app/" />
+				<Card name="QRCode Generator Website" desc="This is a website which can be used for generating qrcode of any links with various sizes." link="https://hqrgenerator.netlify.app/" />
 			</div>
 		</section>
 	)
---
+++ src/Components/Projects/card.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const card = ({ name, desc, link }) => {
+    return (
+        <div className="flex flex-col justify-center items-center">
+            {/* <div className="">
+						<img src="" alt="Logo-Project-1" className="" />
+					</div> */}
+            <div className=" px-3 py-5 flex flex-col justify-center items-center">
+                <h2 className="text-[#a00d20] font-robotoMono text-2xl text-center uppercase underline underline-offset-4 md:text-xl">
+                    {name}
+                </h2>
+                <p className="mt-1 text-white text-sm text-center">
+                    {desc}
+                </p>
+                <button className="mt-2 px-10 w-auto bg-[#a6f8db] font-black rounded-lg">
+                    <a href={link} target="_blank">
+                        View
+                    </a>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default card
