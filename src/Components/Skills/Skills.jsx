cc4e55eed2ed9709d6645114d5c50315fbefb2a9
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-03-07 01:01:26 +0530

    Changed the skills layout from flex to grid

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -18,7 +18,8 @@ const Skills = () => {
 			<h1 className="text-white text-center font-swad text-5xl uppercase">
 				Skills
 			</h1>
-			<div className="flex flex-wrap justify-center items-center">
+			{/* flex flex-wrap justify-center items-center */}
+			<div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 odd:text-red-900 even:text-red-500">
 				<div className="p-1 m-2 text-white flex flex-col justify-center items-center lg:m-4 lg:hover:scale-110 lg:ease-in-out lg:duration-300">
 					<img src={c} alt="C" />
 					<p>
