195dde9387bb3d142f64e3eb8fdc2d670c8b0f43
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-03-07 01:42:56 +0530

    modified projects

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -3,17 +3,14 @@ import Card from './card';
 
 const Projects = () => {
 	return (
-		<section className="p-6 m-5 bg-[#282828] rounded-lg flex flex-col justify-center items-center md:w-5/6 md:mx-auto lg:w-5/6 ">
-			<h1 className="text-white text-center font-swad text-5xl uppercase">
+		<section className="pt-6 pl-6 pr-6 m-5 bg-[#282828] rounded-lg flex flex-col justify-center items-center md:w-5/6 md:mx-auto lg:w-5/6 ">
+			<h1 className="mb-3 text-white text-center font-swad text-5xl uppercase">
 				Projects
 			</h1>
 			<div className="md:grid md:grid-cols-2 lg:grid lg:grid-cols-3 lg:gap-4">
 				<Card name="Country Guide Website" desc="This is a website where you can see the basic detail of any country you can think of." link="https://cguide.netlify.app/" />
 				<Card name="QRCode Generator Website" desc="This is a website which can be used for generating qrcode of any links with various sizes." link="https://hqrgenerator.netlify.app/" />
-				<Card name="QRCode Generator Website" desc="This is a website which can be used for generating qrcode of any links with various sizes." link="https://hqrgenerator.netlify.app/" />
-				<Card name="QRCode Generator Website" desc="This is a website which can be used for generating qrcode of any links with various sizes." link="https://hqrgenerator.netlify.app/" />
-				<Card name="QRCode Generator Website" desc="This is a website which can be used for generating qrcode of any links with various sizes." link="https://hqrgenerator.netlify.app/" />
-				<Card name="QRCode Generator Website" desc="This is a website which can be used for generating qrcode of any links with various sizes." link="https://hqrgenerator.netlify.app/" />
+				<Card name="GitHub CheatSheet Website" desc="This is a website which can be used to refer the GitHub and Git Bash Terminal Commands." link="https://git-cheat-sheet.vercel.app/" />
 			</div>
 		</section>
 	)
