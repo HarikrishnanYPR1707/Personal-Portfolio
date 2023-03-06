9d8456f10bf2004eaf58d1d6d730013b32568390
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-03-07 01:01:01 +0530

    Modified Projects section

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -3,7 +3,7 @@ import Card from './card';
 
 const Projects = () => {
 	return (
-		<section className="p-10 m-5 bg-[#282828] rounded-lg flex flex-col justify-center items-center md:w-5/6 md:mx-auto lg:w-5/6 ">
+		<section className="p-6 m-5 bg-[#282828] rounded-lg flex flex-col justify-center items-center md:w-5/6 md:mx-auto lg:w-5/6 ">
 			<h1 className="text-white text-center font-swad text-5xl uppercase">
 				Projects
 			</h1>
