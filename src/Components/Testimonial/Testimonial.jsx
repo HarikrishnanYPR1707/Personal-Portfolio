06c01242967e924b14833577a619d43c60b12dfb
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-03-10 00:15:21 +0530

    Modified Testimonial.jsx

---
+++ src/Components/Testimonial/Testimonial.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ImQuotesLeft } from 'react-icons/im';
+
+
+const Testimonial = () => {
+    return (
+        <section className="">
+            <ImQuotesLeft className="text-white text-4xl" />
+        </section>
+    )
+}
+
+export default Testimonial
