e5fa78bd584247c1a5f09ae38bd23906778c56a3
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-03-10 17:49:47 +0530

    Completed with the ui

---
+++ src/Components/WorkWithMe/WorkWithMe.jsx
@@ -0,0 +1,37 @@
+import React, { useState} from 'react'
+import emailjs from '@emailjs/browser';
+import workWithMeSvg from '../../assets/workwithme.svg';
+
+const WorkWithMe = () => {
+
+    // Setting State Management Variables
+    const [to_name, setTo_Name] = useState("");
+    const [from_name, setFrom_Name] = useState("");
+    // const [from_email, setFrom_Email] = useState("");
+    const [message, setMessage] = useState("");
+
+    const submitInfo = () => {
+
+    }
+
+    return (
+        <section className="pt-6 pl-6 pr-6 pb-6 m-5 bg-[#282828] rounded-lg flex flex-col justify-center items-center md:w-5/6 md:mx-auto lg:w-5/6 ">
+            <h1 className="mb-3 text-white text-center font-swad text-5xl uppercase">
+                Work With Me
+            </h1>
+            <div className="flex flex-col justify-center items-center">
+                <div className="">
+                    <img src={workWithMeSvg} alt="workWithMeSvg" className="w-72 mb-5" />
+                </div>
+                <div className="flex flex-col justify-center items-center">
+                    <input type="text" id="input-1" className="bg-[#282828] text-white w-[350px] py-2 pl-5 mb-5 rounded-lg border-b-2 border-[#a6f8db] outline-none" placeholder="Enter your Name" onChange={(event) => {setTo_Name(event.target.value)}} required />
+                    <input type="email" id="input-2" className="bg-[#282828] text-white w-[350px] py-2 pl-5 mb-5 rounded-lg border-b-2 border-[#a6f8db] outline-none" placeholder="Enter your Email" onChange={(event) => {setFrom_Email(event.target.value)}} required />
+                    <textarea name="" cols="30" rows="4" id="input-3" className="bg-[#282828] text-white w-[350px] py-2 pl-5 mb-5 rounded-lg border-b-2 border-[#a6f8db] outline-none" placeholder="Enter your Message" onChange={(event) => {setMessage(event.target.value)}} required />
+                    <input type="submit" className="px-10 py-2 bg-[#a6f8db] rounded-md" onClick={submitInfo}/>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default WorkWithMe
